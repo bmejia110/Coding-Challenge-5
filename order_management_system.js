2e15282b2bc0e2ef2768e11b572154a156d8269c
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-20 19:43:18 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -1,6 +1,12 @@
+//Task 1: Create an Orders Array of Order Objects
 let inventory = [
     {name: "espresso", price: 5, quantity: 20}
     {name: "cappuccino", price: 6, quantity: 30}
     {name: "latte", price: 4, quantity: 10}
     {name: "frappucino", price: 7, quantity: 8}
 ]
+
+//Task 2: Create an Orders Array of Order Objects
+
+let orders = []
+
