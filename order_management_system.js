b3ab72238bd4d4b5d902e086ea81c758b250d095
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-20 19:39:45 -0400

    initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,6 @@
+let inventory = [
+    {name: "espresso", price: 5, quantity: 20}
+    {name: "cappuccino", price: 6, quantity: 30}
+    {name: "latte", price: 4, quantity: 10}
+    {name: "frappucino", price: 7, quantity: 8}
+]
