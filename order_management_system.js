92951028a4f2668f28d84d5e41d5019f42d7d2e9
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-22 15:13:41 -0400

    Create calculateOrderTotal funciton

--- order_management_system.js
+++ order_management_system.js
@@ -4,26 +4,34 @@ let inventory = [
     {name: "cappuccino", price: 6, quantity: 30}
     {name: "latte", price: 4, quantity: 10}
     {name: "frappucino", price: 7, quantity: 8}
-]
+];
 
 //Task 2: Create an Orders Array of Order Objects
 
-let orders = []
+let orders = [];
 
 //Task 3: Create a Function to Place an Order
 
-funciton placeorder(customername, itemsordered) {
+function placeorder(customername, itemsordered) {
     let available = itemsordered.every(itemsordered => {
-        let product = inventory.find(p => p.name === itemsordered.name)
-        return product && product.quantity >= itemsordered.quantity
-    })
+        let product = inventory.find(p => p.name === itemsordered.name);
+        return product && product.quantity >= itemsordered.quantity;
+    });
     if (!available)
-        console.log("One or more items not available")
+        console.log("One or more items not available");
     else {
         itemsordered.foreach(itemsordered => {
-            let product = inventory.find(p => p.name === itemsordered.name)
-            product.quantity -= itemsordered.quantity
-        })
-        orders.push({ customername, items: itemsordered, status: "pending"})
+            let product = inventory.find(p => p.name === itemsordered.name);
+            product.quantity -= itemsordered.quantity;
+        });
+        orders.push({ customername, items: itemsordered, status: "pending"});
     }
 }
+
+//Task 4: Create a Fucntion to Calculate Total for an Order
+
+let calculateOrderTotal = order =>
+    order.items.reduce((total, itemsordered) => {
+        let product = inventory.find(product => product.name === itemsordered.name);
+        return total + (product.price * itemsordered.quantity);
+    }, 0);
